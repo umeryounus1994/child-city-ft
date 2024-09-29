bad2673bffa408e0fc6b6e854c09e1cb294f28fe
umeryounus1994 <umeryounus92@gmail.com>
2024-09-30 00:31:28 +0500

    13

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // export const backendUrl = "http://localhost:6969";
-export const backendUrl = "https://child-city-ft-d92b93ded183.herokuapp.com";
+export const backendUrl = "https://child-city-bk-c4c0c9e39c23.herokuapp.com";
 // "https://child-city-backend-a7000291e61e.herokuapp.com";
 export const sizes = ["3-6M", "6-12M", "1-2Y", "2-3Y", "3-4Y"];
